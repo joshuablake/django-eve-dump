cfd18cb140a4e98220f69a576227af664c435fe2
Joshua Blake <joshbblake@gmail.com>
2013-11-20 19:28:54 +0000

    Include all packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name = 'django-eve-dump',
       version = '0.1.0',
       description = 'Models for the EVE static DB',
       author = 'Joshua Blake',
       author_email = 'joshbblake@gmail.com',
-      packages = ['static'],
+      packages = find_packages(),
 )
